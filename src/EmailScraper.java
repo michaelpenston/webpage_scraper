c7ac4c76fe9f02f61951b3a85636f117b87ea79d
mp2g20 <mp2g20@soton.ac.uk>
2020-10-10 15:43:27 +0100

    test update

--- src/EmailScraper.java
+++ src/EmailScraper.java
@@ -3,7 +3,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class EmailScraper {
